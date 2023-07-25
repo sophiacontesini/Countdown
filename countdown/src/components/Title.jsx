b3dff85372755f098046e1c19890e2ae56fa0a3b
Sophia Contesini <sophiamarques214@gmail.com>
2023-07-25 10:50:32 -0300

    feat: change Title

--- countdown/src/components/Title.jsx
+++ countdown/src/components/Title.jsx
@@ -1,10 +1,12 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react';
 import './Title.css';
 
 // eslint-disable-next-line react/prop-types
-export const Title = ({ title }) => {
-  return <h1 className='title'>{title}</h1>;
+const Title = ({ title, eventColor }) => {
+  return (
+    <h1 className='title' style={{ color: eventColor }}>
+      {title}
+    </h1>
+  );
 };
 
 export default Title;
