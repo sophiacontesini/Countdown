7e836e81303ec2eaceb1f47b8214ed620d561874
Sophia Contesini <sophiamarques214@gmail.com>
2023-07-25 08:56:35 -0300

    feat: include title

---
+++ countdown/src/components/Title.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import './Title.css';
+
+// eslint-disable-next-line react/prop-types
+export const Title = ({ title }) => {
+  return <h1 className='title'>{title}</h1>;
+};
+
+export default Title;
